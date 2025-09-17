b13dcf1621b9de547a92b7ee4f0a73454cd8949b
Krishna171205 <sshriyansh827@gmail.com>
2025-09-18 03:36:08 +0530

    adding the analytics part

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
+import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppRoutes />
+      <Analytics/>
     </BrowserRouter>
   )
+  
 }
 
 export default App
