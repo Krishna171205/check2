96044a9ccbd50b30db8208b2bb492cb3d9a6c97e
Krishna171205 <sshriyansh827@gmail.com>
2025-09-18 15:27:47 +0530

    removing the analysis stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
-import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppRoutes />
-      <Analytics/>
+
     </BrowserRouter>
   )
   
